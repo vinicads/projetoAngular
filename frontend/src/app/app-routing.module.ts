f58ae7579ad758a31ac83754986ccb01bc566ca4
Vinicius Augusto <vinicim.augusto@outlook.com>
2025-01-22 16:43:59 -0300

    Criacao de um componente de teste

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Comp2Component } from './comp2/comp2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "comp2",
+    component: Comp2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
