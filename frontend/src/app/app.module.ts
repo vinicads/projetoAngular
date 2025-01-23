c65bc45cd43c07af779e1b55b71d6eb78932cf32
Vinicius Augusto <vinicim.augusto@outlook.com>
2025-01-23 15:46:08 -0300

    Manipulando componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp2Component } from './comp2/comp2.component';
+import { CardComponent } from './components/card/card.component';
+import { Card2Component } from './components/card2/card2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp2Component
+    Comp2Component,
+    CardComponent,
+    Card2Component
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: false,
+  
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+
+}
